cf52f5f5de95fc54b6d8aca6e8d36417a67e00b0
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-10-31 16:31:42 -0600

    first commit

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = '3rinmaes.us.auth0.com'
+export const audience = 'https://bcw-student.com'
+export const clientId = 'jjQ0PXH8N4T1PA6b5yTMdm4rLAWxqx6X'
